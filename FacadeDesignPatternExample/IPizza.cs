40f036b564281bbdbf4d6c3e8ddd46a2b264a394
ewarren1994 <37820139+steelcity412@users.noreply.github.com>
2022-04-22 20:06:55 -0400

    FacadeDesignPattern: Initial setup of the project

---
+++ FacadeDesignPatternExample/IPizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeDesignPatternExample
+{
+    /// <summary>
+    /// This is the interface specific to the Pizza
+    /// </summary>
+    public interface IPizza
+    {
+        void GetVegPizza();
+        void GetNonVegPizza();
+    }
+}
---
+++ FacadeDesignPatternExample/PizzaProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeDesignPatternExample
+{
+    /// <summary>
+    /// This is a pizza provider class which will get pizza for their clients.
+    /// </summary>
+    public class PizzaProvider : IPizza
+    {
+        public void GetNonVegPizza()
+        {
+            GetNonVegPizza();
+            Console.WriteLine("Getting Non Veg Pizza.");
+        }
+
+        public void GetVegPizza()
+        {
+            Console.WriteLine("Getting Veg Pizza.");
+        }
+
+        private void GetNonVegToppings()
+        {
+            Console.WriteLine("Getting Non Veg Pizza Toppings.");
+        }
+        
+    }
+}
